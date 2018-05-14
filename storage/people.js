e350728d7c99ee43d8b0be554bf80bb3e46c3ef0
JP S <github@shipherd.com>
2018-05-14 12:05:25 -0400

    Support lookup by ID

--- storage/people.js
+++ storage/people.js
@@ -11,67 +11,96 @@ const debug = require("debug")("emulator:storage:people");
 
 
 function PersonStorage(datastore) {
-    this.datastore = datastore;
-    this.data = {};
+  this.datastore = datastore;
+  this.data = {};
 }
 
 // Initialize the store with a list of prepared accounts
 PersonStorage.prototype.init = function (accounts) {
-    var self = this;
-    accounts.forEach(function (elem) {
-        self.data[elem.id] = elem;
-    });
-}
+  var self = this;
+  accounts.forEach(function (elem) {
+    self.data[elem.id] = elem;
+  });
+};
 
 PersonStorage.prototype.find = function (actor, personId, cb) {
 
-    assert(actor);
-    assert(personId);
+  assert(actor);
+  assert(personId);
 
-    const person = this.data[personId];
-    if (!person) {
-        debug(`could not find Person with id: ${personId}`);
-        if (cb) {
-            var err = new Error(`could not find Person with id: ${personId}`);
-            err.code = "PERSON_NOT_FOUND";
-            cb(err, null);
-            return;
-        }
-    }
-
+  const person = this.data[personId];
+  if (!person) {
+    debug(`could not find Person with id: ${personId}`);
     if (cb) {
-        cb(null, person);
+      var err = new Error(`could not find Person with id: ${personId}`);
+      err.code = "PERSON_NOT_FOUND";
+      cb(err, null);
+      return;
     }
-}
+  }
+
+  if (cb) {
+    cb(null, person);
+  }
+};
 
 PersonStorage.prototype.findWithEmail = function (actor, personEmail, cb) {
 
-    assert(actor);
-    assert(personEmail);
+  assert(actor);
+  assert(personEmail);
 
-    var found = null;
-    var self = this;
-    Object.keys(this.data).forEach(function(key) {
-        var person = self.data[key];
-        if (person.emails[0] == personEmail) {
-            found = person;
-        }
-    });
-
-    if (!found) {
-        debug(`could not find Person with email: ${personEmail}`);
-        if (cb) {
-            var err = new Error(`could not find Person with email: ${personEmail}`);
-            err.code = "PERSON_NOT_FOUND";
-            cb(err, null);
-        }
-        return;
+  var found = null;
+  var self = this;
+  Object.keys(this.data).forEach(function(key) {
+    var person = self.data[key];
+    if (person.emails[0] == personEmail) {
+      found = person;
     }
+  });
 
+  if (!found) {
+    debug(`could not find Person with email: ${personEmail}`);
     if (cb) {
-        cb(null, found);
+      var err = new Error(`could not find Person with email: ${personEmail}`);
+      err.code = "PERSON_NOT_FOUND";
+      cb(err, null);
     }
-}
+    return;
+  }
+
+  if (cb) {
+    cb(null, found);
+  }
+};
+
+PersonStorage.prototype.findWithId = function (actor, personId, cb) {
+
+  assert(actor);
+  assert(personId);
+
+  var found = null;
+  var self = this;
+  Object.keys(this.data).forEach(function(key) {
+    var person = self.data[key];
+    if (person.id == personId) {
+      found = person;
+    }
+  });
+
+  if (!found) {
+    debug(`could not find Person with id: ${personId}`);
+    if (cb) {
+      var err = new Error(`could not find Person with id: ${personId}`);
+      err.code = "PERSON_NOT_FOUND";
+      cb(err, null);
+    }
+    return;
+  }
+
+  if (cb) {
+    cb(null, found);
+  }
+};
 
 
 module.exports = PersonStorage;
